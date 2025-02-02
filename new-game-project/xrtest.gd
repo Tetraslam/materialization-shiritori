276128576726dc71f69af83e9360024d53a24cc8
Tetraslam <bhowmickshresht@gmail.com>
2025-02-01 21:04:33 -0500

    deleted

--- new-game-project/xrtest.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready():
-	var xr_interface = XRServer.find_interface("OpenXR")
-	
-	if xr_interface and xr_interface.is_initialized():
-		print("✅ OpenXR initialized successfully!")
-	else:
-		print("❌ OpenXR failed to initialize.")
